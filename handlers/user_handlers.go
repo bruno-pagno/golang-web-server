152c9e4ef265e6d2afaab49eaf9b9093b5a2ffc9
Bruno Pagno <pagno.bruno@bcg.com>
2024-03-25 13:46:15 -0300

    added delete and update users method

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"golang-web-server/models"
 	"net/http"
 )
@@ -11,6 +12,31 @@ var db *sql.DB
 
 func SetDatabase(database *sql.DB) {
 	db = database
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Error decoding JSON body", http.StatusBadRequest)
+		return
+	}
+
+	if user.Name == "" {
+		http.Error(w, "Missing 'name'", http.StatusBadRequest)
+		return
+	}
+
+	statement := `INSERT INTO users (name) VALUES ($1) RETURNING id`
+	err = db.QueryRow(statement, user.Name).Scan(&user.Id)
+	if err != nil {
+		http.Error(w, "Error inserting new user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -41,9 +67,14 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var user models.User
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
+		http.Error(w, "Missing 'id' parameter", http.StatusBadRequest)
+		return
+	}
 
+	var user models.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, "Error decoding JSON body", http.StatusBadRequest)
@@ -55,15 +86,33 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	statement := `INSERT INTO users (name) VALUES ($1) RETURNING id`
-	err = db.QueryRow(statement, user.Name).Scan(&user.Id)
+	statement := `UPDATE users SET name = $2 WHERE id = $1`
+	_, err = db.Exec(statement, userID, user.Name)
 	if err != nil {
-		http.Error(w, "Error inserting new user", http.StatusInternalServerError)
+		http.Error(w, "Error updating user", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("id")
+	if userID == "" {
+		http.Error(w, "Missing 'id' parameter", http.StatusBadRequest)
+		return
+	}
+
+	statement := `DELETE FROM users WHERE id = $1`
+	_, err := db.Exec(statement, userID)
+	if err != nil {
+		http.Error(w, "Error deleting user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "User with ID %s deleted successfully", userID)
 }
 
 func UsersRouter(w http.ResponseWriter, r *http.Request) {
@@ -72,6 +121,10 @@ func UsersRouter(w http.ResponseWriter, r *http.Request) {
 		GetUsers(w, r)
 	case http.MethodPost:
 		CreateUser(w, r)
+	case http.MethodPut:
+		UpdateUser(w, r)
+	case http.MethodDelete:
+		DeleteUser(w, r)
 	default:
 		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
 	}
